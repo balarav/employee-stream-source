c6b7fae8afed721f4f16a447439dce4fac4e3175
balarav <rubalaji@gmail.com>
2020-12-24 19:57:11 +0530

    first commit

---
+++ Tasks.md
@@ -0,0 +1,12 @@
+### Tasks
+
+* [x] check if node is installed?
+* [x] check if npm is installed?
+* [x] create a node project inside an empty folder
+    * [x] Documents/employee-api
+* [x] install ExpressJS
+* [x] create an index.js file
+* [x] Write an api method in that file
+* [x] start the web server.
+* [x] return an array of values when GET request.
+
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send("Welcome to landing page of Employee API")
+});
+
+// mock for database.
+const employeeList = [{
+    firstname: "balaji", lastname: "ravichandran"
+}, {
+    firstname: "bala", lastname: "ravi"
+}];
+
+app.get('/employees', (a, b) => {
+    b.send(employeeList)
+});
+
+app.listen(3000, () =>{
+    console.log("server started at port 3000")
+})
